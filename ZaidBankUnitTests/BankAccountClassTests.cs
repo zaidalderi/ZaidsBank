e2db15e094accf3bf5cfbb1b8aac856fe0032d9d
zaidalderi <ZAIDALDERI2001@GMAIL.COM>
2024-12-17 17:43:29 +0300

    first commit

---
+++ ZaidBankUnitTests/BankAccountClassTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using ZaidBank.Models;
+namespace ZaidBankUnitTests;
+
+[TestClass]
+public class BankAccountClassTests
+{
+    [TestMethod]
+    [DataRow("AccountNumber", "accNum")]
+    public void BankAccountShouldHaveTheCorrectDataMemberNames(string propertyName, string dataMemberName)
+    {
+        var property = typeof(BankAccount).GetProperty(propertyName);
+        var dataMember = property.GetCustomAttributes(typeof(DataMemberAttribute), false).First() as DataMemberAttribute;
+        Assert.AreEqual(dataMemberName, dataMember.Name);
+
+        var guid = Guid.NewGuid();
+
+        var date = DateTime.Now;
+
+        Assert.IsNotNull(guid);
+    }
+}
